e427f1ea62b114f2a7a2384749f30fe232aeb80b
moronimotta <dareevil7@gmail.com>
2024-10-18 15:43:35 -0300

    workflow stock

--- check-stock.sh
+++ check-stock.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-URL="https://cse341-ecommerce-project.onrender.com/products/"
+URL="https://cse341-ecommerce-project.onrender.com/products/lowstock"
 
-response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$URL")
+response=$(curl --silent --write-out "%{http_code}" --output /dev/null "$URL")
+products=$(curl --silent "$URL")
 
 current_date=$(date '+%Y-%m-%d %H:%M:%S')
 
-if [ "$response" -ne 200 ]; then
-    message="Message: Health check failed\n\nDate: $current_date"
-    curl -d "$message" ntfy.sh/byu_ecommerce_errors
+if [ "$(echo "$products" | jq length)" -gt 0 ]; then
+    low_stock_items=$(echo "$products" | jq -r '.[] | "Name: \(.name), ID: \(.id)"')
+
+    message="Message: Low stock alert for the following items:
+
+$low_stock_items
+
+Date: $current_date"
+
+    curl -d "$message" ntfy.sh/byu_ecommerce_logs
 else
-    message="Message: Health check OK\n\nDate: $current_date"
+    message="Message: All products are sufficiently stocked
+
+Date: $current_date"
     curl -d "$message" ntfy.sh/byu_ecommerce_logs
 fi
