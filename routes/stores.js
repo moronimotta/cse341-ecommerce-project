e2d792498e2133b4b1d6df2b2858cb9a0f902419
moronimotta <dareevil7@gmail.com>
2024-10-24 15:41:55 -0300

    store authorization OK

--- routes/stores.js
+++ routes/stores.js
@@ -6,10 +6,10 @@ const authorizationChecker = require('../tools/authorization-checker');
 
 router.get('/', storesController.getStores);
 
-router.post('/', authorizationChecker('manager'), storesController.createStore);
+router.post('/', authorizationChecker('admin'), storesController.createStore);
 router.put('/:id', authorizationChecker('manager'), storesController.updateStore);
 router.delete('/:id', authorizationChecker('manager'), storesController.deleteStore);
 
-router.get('/:id', authorizationChecker('customer'), storesController.getStore);
+router.get('/:id', storesController.getStore);
 
 module.exports = router;
