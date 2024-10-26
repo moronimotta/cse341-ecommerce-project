42d4b3261f86d97e4522787e04649d582976e995
ndamatta <damatta.natanael@gmail.com>
2024-10-26 12:19:52 -0300

    finish orders week 2

--- controllers/orders.js
+++ controllers/orders.js
@@ -39,27 +39,30 @@ const getSingleOrder = async (req, res) => {
   };
 
   const createOrder = async (req, res) => {
-    const orderData = {
-      user_id: req.body.user_id,
-      cart_id: req.body.cart_id,
-      amount: req.body.amount,
-      status: req.body.status,
-    };
+    const orderData = { ...req.body, status: 'pending' };
   
     try {
-
+      const database = await mongodb.getDb();
+  
+      // Check if store_id exists
       if (orderData.store_id) {
-        const store = await storeController.getStore(orderData.store_id);
+        const store = await database.collection('stores').findOne({ _id: new ObjectId(orderData.store_id) });
         if (!store) {
           return res.status(400).json({ message: 'Store not found' });
+        }
+      }
+  
+      // Check if cart_id exists
+      if (orderData.cart_id) {
+        const cart = await database.collection('carts').findOne({ _id: new ObjectId(orderData.cart_id) });
+        if (!cart) {
+          return res.status(400).json({ message: 'Cart not found' });
         }
       }
 
       const newOrder = new Order(orderData);
       await newOrder.validate();
   
-      // Using mongodb native methods as it's easier for me, but validating with mongoose.
-      const database = await mongodb.getDb();
       const response = await database.collection('orders').insertOne(orderData);
   
       if (response.acknowledged) {
@@ -158,5 +161,51 @@ const getSingleOrder = async (req, res) => {
       res.status(500).json({ message: error.message || 'An error occurred while fetching the order.' });
     }
   }
+
+  const payOrder = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Must be a valid Order ID to update payment status');
+    }
   
-  module.exports = {getAllOrders, getSingleOrder, createOrder, updateOrder, deleteOrder, getAllOrdersByStoreId, getAllOrdersByUserId};
+    try {
+      const orderId = new ObjectId(req.params.id);
+      const database = await mongodb.getDb();
+  
+      const response = await database.collection('orders').updateOne({ _id: orderId },{ $set: { status: 'paid' } });
+  
+      if (response.modifiedCount > 0) {
+        res.status(200).json({ message: 'Order status updated to paid' });
+      } else if (response.matchedCount === 0) {
+        res.status(404).json({ message: 'Order not found' });
+      } else {
+        throw new Error('Order is already paid');
+      }
+    } catch (error) {
+      sendNotification(error, 'system_error');
+      res.status(500).json({ error: error.message || 'An unknown error occurred' });
+    }
+  };
+
+  const refundOrder = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Must be a valid Order ID to refund an order');
+    }
+  
+    try {
+      const orderId = new ObjectId(req.params.id);
+      const database = await mongodb.getDb();
+  
+      const response = await database.collection('orders').updateOne({ _id: orderId },{ $set: { status: 'refund', amount: 0 } });
+  
+      if (response.modifiedCount > 0) {
+        res.status(200).json({ message: 'Order status refunded' });
+      } else {
+        res.status(500).json({ message: 'Failed to update order status to refund' });
+      }
+    } catch (error) {
+      sendNotification(error, 'system_error');
+      res.status(500).json({ error: error.message || 'An unknown error occurred' });
+    }
+  };
+  
+  module.exports = {getAllOrders, getSingleOrder, createOrder, updateOrder, deleteOrder, getAllOrdersByStoreId, getAllOrdersByUserId, payOrder, refundOrder};
--- routes/orders.js
+++ routes/orders.js
@@ -5,18 +5,19 @@ const authorizationChecker = require('../tools/authorization-checker');
 
 
 // Admins
-router.get('/', ordersController.getAllOrders);
+router.get('/',authorizationChecker('admin'), ordersController.getAllOrders);
 
 // Only admins and managers (managers only change their store orders)
 router.post('/', ordersController.createOrder);
-router.put('/:order_id', ordersController.updateOrder);
-router.delete('/:order_id', ordersController.deleteOrder);
+router.put('/:order_id', authorizationChecker('manager'), ordersController.updateOrder);
+router.delete('/:order_id', authorizationChecker('manager'), ordersController.deleteOrder);
+router.put('/refund/:id', authorizationChecker('manager'), ordersController.refundOrder)
+router.put('/pay/:id', authorizationChecker('manager'), ordersController.payOrder)
 
 // Customer can see his orders.
-router.get('/:order_id', ordersController.getSingleOrder);
-
-router.get('/store/:id', ordersController.getAllOrdersByStoreId);
-router.get('/user/:id', ordersController.getAllOrdersByUserId);
+router.get('/:order_id', authorizationChecker('customer'), ordersController.getSingleOrder);
+router.get('/store/:id', authorizationChecker('customer'), ordersController.getAllOrdersByStoreId);
+router.get('/user/:id', authorizationChecker('customer'), ordersController.getAllOrdersByUserId);
 
 
 module.exports = router;
